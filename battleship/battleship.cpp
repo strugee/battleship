dafd15e7db2295eea7269b78c912467a5d276e16
Alex Jordan <alexander3223098@gmail.com>
2012-01-02 15:09:08 -0800

    Start the project, basic header and source file

---
+++ battleship/battleship.cpp
@@ -0,0 +1,14 @@
+// Battleship program
+// Written by Alex Jordan
+
+#include <iostream>
+#include <battleship.h>
+using namespace std;
+
+int main()
+{
+	cout << "Initializing... ";
+	battleship game;
+	cout << "Done. Unfortunately, nothing else can be done. Bye!";
+	return 0;
+}
---
+++ battleship/bin/Debug/battleship.d
@@ -0,0 +1,4 @@
+ \
+ /Users/alex/Development/github/battleship/battleship/bin/Debug/battleship.o:  \
+ /Users/alex/Development/github/battleship/battleship/battleship.cpp \
+  /users/alex/development/github/battleship/battleship/battleship.h
