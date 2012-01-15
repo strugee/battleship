22b1eedb833abf9d66bea620cbaa24988833d222
Alex Jordan <alexander3223098@gmail.com>
2012-01-14 20:18:20 -0800

    various changes - (almost!) finish the needed game classes mostly
    battleship.cpp -
    *change the battleship object name from game to battleship
    *#included cstdlib
    *add reminder to only ever interact w/ the battleship object directly
    (e.g. battleship.method(args) instead of battleship.player1.method())
    
    battleship.h -
    *#include iostream and cstdlib
    *add board class, which exposes methods to get a value for a specified
    coordinate and/or update a coordinate's value, and also contains an
    array to hold the board values
    *add player class, which creates two instances of the board class (two
    boards per player)
    *almost finish battleship class
    **expose method to change the active player
    **expose method to render the board (incomplete, will not work right
    now!)
    **expose method to update the board (this will call updateinternalboard
    in the desired board object)
    **contains a variable to keep track of the active player
    **initializes two player objects

--- battleship/battleship.cpp
+++ battleship/battleship.cpp
@@ -1,14 +1,17 @@
 // Battleship program
 // Written by Alex Jordan
 
+// Note: you should only ever interact with battleship.* in this. The battleship class will handle everything else.
+
 #include <iostream>
+#include <cstdlib>
 #include "battleship.h"
 using namespace std;
 
 int main()
 {
 	cout << "Initializing... ";
-	battleship game;
+	battleship battleship;
 	cout << "Done. Unfortunately, nothing else can be done. Bye!";
 	return 0;
 }
--- battleship/battleship.h
+++ battleship/battleship.h
@@ -1,11 +1,94 @@
 // Battleship class structure
 // Written by Alex Jordan
 
+//#include <iostream>
+//#include <cstdlib>
+
+// header guard - so compilers don't throw errors if this is #included more than once
 #ifndef __BATTLESHIP_H_INCLUDED__
 #define __BATTLESHIP_H_INCLUDED__
 
-class battleship {
-	
+#include <iostream>
+#include <cstdlib>
+
+class board {
+	public:
+		void updateinternalboard(int coordinate1, int coordinate2, int value) {
+			board[coordinate1][coordinate2] = value;
+		}
+	private:
+		int getCoordinateValue(int coordinate1, int coordinate2) {
+			return board[coordinate1][coordinate2];
+		}
+		int board[9][9];
 }
 
+class player {
+	public:
+	
+	private:
+		board ships; // this board holds the ships of the player.
+		board shots; // this board tracks the player's shots at the other player's ships
+}
+
+class battleship {
+	public:
+		void changeActivePlayer(int input) {
+			if (input == 0) {
+				if (activePlayer == 1) {
+					activePlayer = 2;
+				}
+				if (activePlayer == 2) {
+					activePlayer = 1;
+				}
+			}
+			if (input == 1) {
+				activePlayer = 1;
+			}
+			if (input == 2) {
+				activePlayer = 2;
+			}
+		}
+		
+		void render() {
+			if (activePlayer == 1) {
+				for (int coordinate1 = 0; coordinate1 <= 10; coordinate1++) {
+					cout << "-----" << "-----" << "-----" << "-----" << "-----" << "-----" << "-----" << "-----" << "-----" << "-----";
+					for (int coordinate2; coordinate2 <= 10; coordinate2++) {
+						cout << "| " << coordinate2 << " ";
+						if (coordinate2 == 10) {
+							cout << "|"
+						}
+					}
+				}
+				cout << "-----" << "-----" << "-----" << "-----" << "-----" << "-----" << "-----" << "-----" << "-----" << "-----";
+			}
+		}
+		
+		void updateboard(int coordinate1, int coordinate2, int boardID, int value) {
+			if (boardID == 1) {
+				// if it's player 1's board with player 1's ships
+				player1.ships.updateinternalboard(coordinate1, coordinate2, value);
+			}
+			if (boardID == 2) {
+				// if it's player 1's board tracking player 1's shots
+				player1.shots.updateinternalboard(coordinate1, coordinate2, value);
+			}
+			if (boardID == 3) {
+				// if it's player 2's board with player 2's ships
+				player2.ships.updateinternalboard(coordinate1, coordinate2, value);
+			}
+			if (boardID == 4) {
+				// if it's player 2's board tracking player 2's shots
+				player2.ships.updateinternalboard(coordinate1, coordinate2, value);
+			}
+		}
+		
+	private:
+		int activePlayer = 1;
+		player player1;
+		player player2;
+}
+
+// end header guard
 #endif
